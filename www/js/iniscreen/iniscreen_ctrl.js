7fc76d2942f49bb3c1bbbd8b96628dac67cb9bca
soliseyoum <56610159+soliseyoum@users.noreply.github.com>
2019-12-06 11:56:43 -0500

    Update iniscreen_ctrl.js

--- www/js/iniscreen/iniscreen_ctrl.js
+++ www/js/iniscreen/iniscreen_ctrl.js
@@ -15,24 +15,7 @@ angular.module('starter.controllers')
 	'use strict';
 	$ionicSideMenuDelegate.canDragContent(false); // hide sidemenu
 	
-	$scope.getAddress = function(attrs){
-			progressService.showLoader();
-			var geocoder = new google.maps.Geocoder();
-			var latlng = new google.maps.LatLng(attrs.lat, attrs.lng);
-			geocoder.geocode({ 'latLng': latlng }, function (results, status) {
-				if (status == google.maps.GeocoderStatus.OK) {
-					if (results[1]) {
-						$scope.userAddress = results[1].formatted_address;
-						progressService.hideLoader();
-						$timeout(function(){ $location.path("app/dashboard");  }, 1500);
-					} else {
-						$scope.userAddress = 'Location not found';
-					}
-				} else {
-					$scope.userAddress = 'Geocoder failed due to: ' + status;
-				}
-			});
-	}
+
 
 	$scope.findLocation = function(){
 		  var posOptions = {timeout: 10000, enableHighAccuracy: false};
@@ -75,12 +58,11 @@ angular.module('starter.controllers')
 		var lng  = data.geometry.location.lng();
 		var lat  = data.geometry.location.lat();
 		var lnglat = {lng:lng,lat:lat};
-		$scope.iniRegister.city  = $scope.userLocation.formatted_address;
 	}
-	$scope.cuntryData = [{country_id:'',name:'-- Select Country --'},{country_id:'99',name:'India'}];
+	$scope.cuntryData = [{country_id:'',name:'-- Select HBCU --'},{country_id:'1',name:'Howard University'},{country_id:'2',name:'Florda A&M University'},{country_id:'3',name:'Morehouse College/Spelman College/Clark Atlanta University'},{country_id:'4',name:'North Carolina A&T University'},{country_id:'5',name:'Tuskegee University'}];
 
 
-	$scope.iniRegister = {firstname:'', lastname:'',telephone:'',postcode:'',country_id:'',city:'',address_1:'',email:'',password:'',confirm:'',zone_id:'1433',agree:'1'}; 
+	$scope.iniRegister = {firstname:'', lastname:'',telephone:'',postcode:'',country_id:'',email:'',password:'',confirm:'',zone_id:'1433',agree:'1'}; 
 	
 	$scope.userRegister = function(form){
 		if(form.$valid) {			
